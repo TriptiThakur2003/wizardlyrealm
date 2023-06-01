61d1932f8d3fdfd87a7268ee4fb1de6bbd1c10da
tripti <thakurtripti2003@gmail.com>
2023-06-01 22:47:12 +0530

    first

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" class="scroll-smooth">
+<head>
+    <title>Hello</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- bootstrap -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous">
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+
+    <!-- bootstrap icon -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- google icons -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+    />
+
+    <!-- boxicons -->
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Lobster&family=Nanum+Gothic&family=Playfair+Display&family=Poppins:wght@200;400;500&family=Roboto+Slab&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</head>
+<style>
+    .container{
+      display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .body_container{
+        background-image: linear-gradient(orangered, yellow, greenyellow);
+    }
+</style>
+    <div class="body_container">
+        <div class="container">
+            <div class="card card-body">
+                <h1>hello how are you</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer strat -->
+    <section id="footer">
+        <!--                <img src="assets/footerbg.png" alt="" style="width: 100%; " >-->
+        <div class="container " id="main_footer_container">
+            <div class="row " id="main_footer">
+                <div class="col-4 " id="footer_item1">
+                    <div class="row">
+                        <div class="col my-2 text-secondary"><h6>Company</h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="About_us.php">About</a></h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="policy.php">Privacy policy</a></h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="terms_conditions.php">Terms and Conditions</a></h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="investor_us_form.php">Investor</a></h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="careers.php">Careers</a></h6></div>
+                    </div>
+                </div>
+                <div class="col-4 " id="footer_item2" >
+                    <div class="row">
+                        <div class="col my-2 text-secondary"><h6>Services</h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="bookingengine.php">Booking Engine</a></h6></div>
+                    </div>
+                    <div class="row">
+                        <div class="col my-2"><h6><a href="websitebuilder.php">Website Builder</a></h6></div>
+                    </div>
+                </div>
+                <div class="col-md-4 offset-md-4" id="footer_item3">
+                    <h4></h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- footer end -->
+
+    <!-- ====== Back To Top Start -->
+    <section id="back_to_top"class="p-2">
+        <div class="container d-flex justify-content-center" >
+            <div class="row align-items-center" id="policy">
+                <div class="col d-flex">
+                    <p>© 2023 SaasAro, All Right Reserved</p>
+                    <a href="https://www.facebook.com/official.saasaro" target="_blank"><i class="bi bi-facebook px-2"></i></a>
+                    <a href="" target="_blank"><i class="bi bi-twitter px-2" ></i></a>
+                    <a href="https://www.instagram.com/official_saasaro/" target="_blank"><i class="bi bi-instagram px-2"></i></a>
+                    <a href="https://www.linkedin.com/company/saasaro/?viewAsMember=true" target="_blank"><i class="bi bi-linkedin px-2"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mx-2">
+                <a href="mailto:<nowiki>bookings@saasaro.com?subject=subject text"  id="bookings_mail">bookings@saasaro.com</a>
+            </div>
+        </div>
+    </section>
+    <!-- ====== Back To Top end -->
+</body>
+
+</html>
